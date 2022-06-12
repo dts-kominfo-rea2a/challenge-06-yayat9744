57b5d0de4d00837ef8109af4243af6d73f564e72
yayat9744 <yayat9744@gmail.com>
2022-06-12 18:09:38 +0700

    add-challenge-06-yayat

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,8 +18,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
-
+const bacaData = (fnCallback) => {
+  let data = [];
+  fs.readFile(file1, "utf-8", (err, data1) => {
+    if (err) return fnCallback(err);
+    data.push(pemisahKata(JSON.parse(data1)));
+    fs.readFile(file2, "utf-8", (err, data2) => {
+      if (err) return fnCallback(err);
+      data.push(pemisahKata(JSON.parse(data2)[0]));
+      fs.readFile(file3, "utf-8", (err, data3) => {
+        if (err) return fnCallback(err);
+        data.push(pemisahKata(JSON.parse(data3)[0].data));
+        fnCallback(null, data);
+      });
+    });
+  });
+};
+const pemisahKata = (data) => {
+  let _data = data.message.split(" ");
+  return _data[1];
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
